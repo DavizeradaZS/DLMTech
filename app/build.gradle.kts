0f7dd0e6701482a1f8dcbc0b355d9a8c69242fcc
marcus.vgsampaio <marcuss.sampaioo@gmail.com>
2026-05-05 18:32:57 -0300

    fix - projeto ronandao precisa intalar o android 14 (api34)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.dlmtech"
-    compileSdk = 36
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.dlmtech"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
 
 
