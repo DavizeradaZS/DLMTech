84f06f4515e9b6c034a06db5d4805520f80bee03
MarcusSampaio <marcuss.sampaioo@gmail.com>
2026-05-07 23:04:50 -0300

    update - tela de produto ok - falta os botões de navegação

--- app/src/main/java/com/example/dlmtech/Activity_produto.kt
+++ app/src/main/java/com/example/dlmtech/Activity_produto.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class activity_produto : AppCompatActivity() {
+class Activity_produto : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
