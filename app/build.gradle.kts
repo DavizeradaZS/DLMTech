beea9d87366239b0ce457aa21c4ea5fa88107f12
Tedesche <leandro.tedesche@outlook.com>
2026-05-09 21:32:59 -0300

    att dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
