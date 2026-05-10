280268fb5ea709bc019cf47dab4567c133b73b31
MarcusSampaio <marcuss.sampaioo@gmail.com>
2026-05-10 09:29:39 -0300

    add - inclusão das dependencias e config para a Recyleview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
+    implementation(libs.androidx.recyclerview)
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
