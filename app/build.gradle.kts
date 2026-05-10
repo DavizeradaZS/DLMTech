3f7be73859933f65ae87613c1fb22474b4b4906b
Tedesche <leandro.tedesche@outlook.com>
2026-05-10 00:47:20 -0300

    telas de cadastro

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.legacy.support.v4)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
