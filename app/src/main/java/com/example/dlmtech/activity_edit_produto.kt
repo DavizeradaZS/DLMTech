ba3ee1c7250c9e3dc38c20a4e2cc801d8fa38fe4
Tedesche <leandro.tedesche@outlook.com>
2026-05-28 16:16:17 -0300

    tela de edição de produto

---
+++ app/src/main/java/com/example/dlmtech/activity_edit_produto.kt
@@ -0,0 +1,20 @@
+package com.example.dlmtech
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class activity_edit_produto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_edit_produto)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
