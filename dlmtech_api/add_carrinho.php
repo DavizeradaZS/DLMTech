e2e8ab2bba90d9c972ff510c8dd2dfa147b97f22
MarcusSampaio <marcuss.sampaioo@gmail.com>
2026-06-02 21:53:53 -0300

    update- ajustes do novo campo de cliente em carrinho

--- dlmtech_api/add_carrinho.php
+++ dlmtech_api/add_carrinho.php
@@ -1,6 +1,10 @@
 <?php
 include 'config.php';
-$produto_id = $_POST['produto_id'];
-$sql = "INSERT INTO carrinho (produto_id) VALUES ('$produto_id')";
+
+$produto_id = (int) $_POST['produto_id'];
+$cliente_id = (int) $_POST['cliente_id'];
+
+$sql = "INSERT INTO carrinho (produto_id, cliente_id) VALUES ($produto_id, $cliente_id)";
+
 echo json_encode(["sucesso" => $conn->query($sql)]);
 ?>
